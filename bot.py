3e7c0d7fcf6eebe4dc1e85aaff054898a24b9f5f
Delta Burnt <deltaburnt@gmail.com>
2014-01-05 20:07:58 -0600

    Main bot class added

--- bot.py
+++ bot.py
@@ -3,31 +3,38 @@
 #TODO: flesh out irc.py api
 
 import net.irc, api.scheduler, api.loader
-import string, time, sqlite3
+import sys, string, time, sqlite3, ConfigParser
 
-conn = sqlite3.connect('bot.db')
+class Bot():
+	def __init__(self):
+		conn = sqlite3.connect('bot.db')
 
-freenode = net.irc.IRC()
-freenode.connect('irc.quakenet.org', 6667, 'DeltaburntBot', 'My bot', ['#deltaburnt'])
+		freenode = net.irc.IRC()
+		freenode.connect('irc.quakenet.org', 6667, 'DeltaburntBot', 'My bot', ['#deltaburnt'])
 
-scheduler = api.scheduler.Scheduler()
-loader = api.loader.Loader(scheduler, freenode, conn)
-loader.load_all()
+		scheduler = api.scheduler.Scheduler()
+		loader = api.loader.Loader(scheduler, freenode, conn)
+		loader.load_all()
 
 
-while True:
-	for line in freenode.poll():
-		line = string.rstrip(line)
-		#not whitespace
-		if line:
-			response = net.irc.Response.parse(line)
-			if response['type'] != 'unknown':
-				if response['data']: data = response['data']
-				else: data = None
-				scheduler.call_event(response['type'], data)
-				conn.commit()
-			#print response
-			if response['type'] == 'connect':
-				freenode.join()
-			if response['type'] == 'ping':
-				freenode._send_raw(net.irc.Commands.PONG(response['data']['sender']))
+	def run():
+		while True:
+			for line in freenode.poll():
+				line = string.rstrip(line)
+				#not whitespace
+				if line:
+					response = net.irc.Response.parse(line)
+					if response['type'] != 'unknown':
+						if response['data']: data = response['data']
+						else: data = None
+						scheduler.call_event(response['type'], data)
+						conn.commit()
+					#print response
+					if response['type'] == 'connect':
+						freenode.join()
+					if response['type'] == 'ping':
+						freenode._send_raw(net.irc.Commands.PONG(response['data']['sender']))
+
+if __name__ == '__main__':
+	bot = Bot()
+	bot.run()
