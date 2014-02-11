55e400d24f1b50ce64a8ff73edda11a16af164b2
Delta Burnt <deltaburnt@gmail.com>
2014-02-11 11:45:21 -0600

    More plugin return results, plugins can now prevent default functionality of bot

--- api/plugin.py
+++ api/plugin.py
@@ -8,7 +8,9 @@ class Priority():
 	LOG = 1
 	
 class Result():
-	PREVENT_ALL = 1
+	PREVENT_ALL = 3
+	PREVENT_DEFAULT = 2
+	PREVENT_PLUGINS = 1
 	SUCCESS = None
 
 #Abstract class, do not instantiate
